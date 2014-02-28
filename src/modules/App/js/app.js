4cb3b974d78474a00ecb3b913438920ef6a52e2f
Roger Dudler <roger.dudler@gmail.com>
2014-02-28 15:49:40 +0100

    updated routing and introduced layout configuration.

--- src/modules/App/js/app.js
+++ src/modules/App/js/app.js
@@ -1,9 +1,12 @@
 (function($) {
   Tc.Module.App = Tc.Module.extend({
 
-    views: [],
     $layout: null,
-    loading: false,
+
+    views: [],
+    routes: [],
+    layouts: [],
+    currentView: null,
 
     on: function(callback) {
 
@@ -13,8 +16,16 @@
         // Override click events for any links, and call Path.history.pushState
         // method to invoke the PathJS router.
         $ctx.on('click', 'a', function(event) {
-            event.preventDefault();
-            Path.history.pushState({path: $(this).attr('href')}, $(this).children('span').text(), $(this).attr('href'));
+            var route = $(this).attr('href');
+            if (that.routes[route]) {
+                event.preventDefault();
+                var title = that.views[that.routes[route]]['title'];
+                Path.history.pushState(
+                    { path: route }, 
+                    title, 
+                    route
+                );
+            }
         });
 
         callback();
@@ -30,14 +41,24 @@
             method: 'GET',
             dataType: 'json',
             success: function(data) {
+
+                // initialize route mappings
                 $.each(data.views, function(key, view) {
                     that.views[view.id] = view;
+                    that.routes[view.route] = view.id;
                     Path.map(view.route).to(function() {
                         that.view(view.id);
                     });
                 });
-                // Listen to history and fallback to hashtags if HTML5 is not supported
+
+                // load layout configurations
+                $.each(data.layouts, function(key, layout) {
+                    that.layouts[layout.id] = layout;
+                });
+
+                // listen to history and fallback to hashtags if HTML5 is not supported
                 Path.history.listen(true);
+                Path.history.popState();
             }
         });
     },
@@ -47,42 +68,57 @@
     },
 
     view: function(id) {
+        
         var that = this;
         var view = this.views[id];
         var layout = this.$layout.data('layout');
-        document.title = view.title;
 
-        var new_layout = view.layout != null ? view.layout : 'iframe';
-        if (layout != new_layout) {
-            var $layout = $(tmpl['layout-' + new_layout]({}));
+        // set current view id
+        this.currentView = id;
+        document.title = view['title'];
+
+        // if there is a layout change, initialize it
+        if (layout != view.layout) {
+            var $layout = $(tmpl['layout-' + view.layout]({}));
             that.$layout.empty().append($layout);
-            that.$layout.data('layout', new_layout);
+            that.$layout.data('layout', view.layout);
             that.$layout.removeClass();
-            that.$layout.addClass('mod mod-layout skin-layout-' + new_layout);
+            that.$layout.addClass('mod mod-layout skin-layout-' + view.layout);
+
+            // initialize layout
+            $.each(this.layouts[view.layout].modules, function(region, modules) {
+                var $region = $('[data-region="' + region + '"]', that.$layout);
+                $region.empty();
+                $.each(modules, function(index, module_id) {
+                    if (tmpl[module_id]) {
+                        $module = $(tmpl[module_id]({}));
+                        $region.append($module);
+                    } else {
+                        console.log('module "' + module_id + '" does not exist - skipped');
+                    }
+                });
+                that.sandbox.addModules($region);
+            });
+
         }
 
         // render modules into regions
-        var new_modules = [];
         if (view.modules) {
             $.each(view.modules, function(region, modules) {
-                if (false && layout == new_layout && (region == 'header' || region == 'footer')) {
-                    // noop
-                } else {
-                    var $region = $('[data-region="' + region + '"]', that.$layout);
-                    $region.empty();
-                    $.each(modules, function(index, module_id) {
-                        if (tmpl[module_id]) {
-                            $module = $(tmpl[module_id]({}));
-                            $region.append($module);
-                            new_modules.push($module);
-                        } else {
-                            console.log('module "' + module_id + '" does not exist - skipped');
-                        }
-                    });
-                    that.sandbox.addModules($region);
-                }
+                var $region = $('[data-region="' + region + '"]', that.$layout);
+                $region.empty();
+                $.each(modules, function(index, module_id) {
+                    if (tmpl[module_id]) {
+                        $module = $(tmpl[module_id]({}));
+                        $region.append($module);
+                    } else {
+                        console.log('module "' + module_id + '" does not exist - skipped');
+                    }
+                });
+                that.sandbox.addModules($region);
             });
         }
+        
     }
 
   });
