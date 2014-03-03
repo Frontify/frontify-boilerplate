0c54adac843a58dd814834e93013334e45ccdfeb
Roger Dudler <roger.dudler@gmail.com>
2014-03-03 23:53:40 +0100

    fixed test.

--- test/nightwatch.js
+++ test/nightwatch.js
@@ -7,7 +7,7 @@ module.exports = {
       .waitForElementVisible('.mod-navigation', 1000)
       .waitForElementVisible('.mod-navigation .item:nth-of-type(2)', 1000)
       .click('.mod-navigation .item:nth-of-type(2) a')
-      .assert.title('Users')
+      .assert.title('User')
       .waitForElementVisible('.mod-article', 1000)
       .assert.containsText('.mod-article', 'User Content')
       .click('.mod-navigation .item:nth-of-type(1) a')
